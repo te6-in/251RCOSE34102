33c280205b2129a4029f1e9ccc58e0ceeaa514a9
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-05-20 08:06:07 +0000

    Fix processes with I/O request time set to 0 not starting I/O immediately

--- src/executor.c
+++ src/executor.c
@@ -41,39 +41,46 @@ static void tick(Scheduler *scheduler, ProcessQueue *io_queue, int *current_time
 
 void execute_one_tick(Scheduler *scheduler, ProcessQueue *io_queue, Process **running_process,
                       int *current_time) {
-  // 현재 실행 중인 프로세스가 I/O 들어가야 하는 경우
-  if (*running_process && ((*running_process)->is_in_io) == false &&
-      (*running_process)->io_burst_remaining > 0 &&
-      ((*running_process)->cpu_burst - (*running_process)->cpu_burst_remaining ==
-       (*running_process)->io_request_time)) {
-    print_time(*current_time);
-    printf("프로세스 %d I/O 요청 시작 (burst %d 남음, I/O %d 예정)\n", (*running_process)->pid,
-           (*running_process)->cpu_burst_remaining, (*running_process)->io_burst_remaining);
-
-    (*running_process)->is_in_io = true;
-    enqueue(io_queue, *running_process);
-    *running_process = NULL; // running_process가 I/O 큐에 들어가면 running_process는 NULL
-  }
-
-  // 현재 실행 중인 프로세스가 원래 없었거나, I/O 큐에 들어가서 NULL이 되었을 수 있음
-  if (!*running_process) {
-    // 새로 꺼내기
-    *running_process = scheduler->pick_next(scheduler);
-
-    // 스케줄러가 줄 수 있는 프로세스가 없는 경우
+  // running_process 찾고, 즉시 I/O 요청을 해야 하면 I/O 큐에 넣고 다시 고르기
+  while (1) {
+    // 없으면 꺼냄
     if (!*running_process) {
-      print_duration(*current_time, *current_time + 1);
-      printf("IDLE\n");
+      *running_process = scheduler->pick_next(scheduler);
 
-      record_idle_entry();
-      tick(scheduler, io_queue, current_time);
+      // 스케줄러가 줄 수 있는 프로세스가 없는 경우 IDLE로 execute_one_tick() 종료
+      if (!*running_process) {
+        print_duration(*current_time, *current_time + 1);
+        printf("IDLE\n");
 
-      // IDLE 완료, 틱 종료
-      return;
+        record_idle_entry();
+        tick(scheduler, io_queue, current_time);
+
+        return;
+      }
+
+      // 성공적으로 꺼낸 경우
+      (*running_process)->waiting += *current_time - (*running_process)->last_ready_enqueued_at;
     }
 
-    // 성공적으로 꺼낸 경우
-    (*running_process)->waiting += *current_time - (*running_process)->last_ready_enqueued_at;
+    // (새로) 선택된 프로세스가 I/O 요청을 해야 하는 경우
+    if ((*running_process)->is_in_io == false && (*running_process)->io_burst_remaining > 0 &&
+        ((*running_process)->cpu_burst - (*running_process)->cpu_burst_remaining ==
+         (*running_process)->io_request_time)) {
+
+      print_time(*current_time);
+      printf("프로세스 %d I/O 요청 시작 (burst %d 남음, I/O %d 예정)\n", (*running_process)->pid,
+             (*running_process)->cpu_burst_remaining, (*running_process)->io_burst_remaining);
+
+      (*running_process)->is_in_io = true;
+      enqueue(io_queue, *running_process);
+
+      // I/O로 보냈으므로 새로 뽑아야 함
+      *running_process = NULL;
+      continue;
+    }
+
+    // 실행 가능한 running_process
+    break;
   }
 
   // 처음 실행하는 프로세스
