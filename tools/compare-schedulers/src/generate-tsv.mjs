ce404311d8fda07a4374ef766083bfb7f5468b38
Joo Chanhwi <56245920+te6-in@users.noreply.github.com>
2025-05-22 10:35:57 +0000

    Generate TSVs with random values

---
+++ tools/compare-schedulers/src/generate-tsv.mjs
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import path from 'path';
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateProcesses(processCount) {
+  let lines = [
+    "# pid  arrived_at   priority   cpu_burst  io_burst   io_request_time"
+  ];
+
+  for (let i = 0; i < processCount; i++) {
+    const pid = 10000 + i;
+    const arrived_at = getRandomInt(0, 500);
+    const priority = getRandomInt(1, 10);
+    const cpu_burst = getRandomInt(1, 500);
+    const io_burst = getRandomInt(0, 1) === 0 ? 0 : getRandomInt(1, 100);
+    const io_request_time = io_burst > 0 ? getRandomInt(0, cpu_burst) : 0;
+
+    lines.push(`${pid} ${arrived_at} ${priority} ${cpu_burst} ${io_burst} ${io_request_time}`);
+  }
+
+  return lines.join('\n') + '\n';
+}
+
+function generateTsv({ fileCount, processCount }) {
+  const dir = '../../data/__generated__/';
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  for (let i = 0; i < fileCount; i++) {
+    const fileName = path.join(dir, `processes_${i}.tsv`);
+    const data = generateProcesses(processCount);
+
+    fs.writeFileSync(fileName, data);
+    console.log(`Generated ${fileName}`);
+  }
+}
+
+generateTsv({ fileCount: 100, processCount: 30 });
